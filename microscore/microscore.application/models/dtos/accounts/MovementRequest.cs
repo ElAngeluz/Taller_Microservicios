744f7dd1f05e53c5149eea23a7a7510801704a65
Pedro Abel <privera@ansofttech.net>
2023-10-24 07:42:36 -0500

    clases de movimiento

---
+++ microscore/microscore.application/models/dtos/accounts/MovementRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace microscore.application.models.dtos.accounts
+{
+    public class MovementRequest
+    {
+        public string AccountNumber { get; set; }
+        public string TypeAccount { get; set; }
+        public string BalanceTrx { get; set; }
+        public bool State { get; set; }
+        public string Movement { get; set; }
+    }
+}
